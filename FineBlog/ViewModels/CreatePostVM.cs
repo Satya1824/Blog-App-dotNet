950acd9050516817b6613a1fda28146589bdeab6
Satya Prakash <satsou24@gmail.com>
2024-05-03 09:18:57 +0530

    final commit

---
+++ FineBlog/ViewModels/CreatePostVM.cs
@@ -0,0 +1,17 @@
+﻿using FineBlog.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FineBlog.ViewModels
+{
+    public class CreatePostVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ApplicationUserId { get; set; }
+        public string? Description { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public IFormFile? Thumbnail { get; set; }
+    }
+}
